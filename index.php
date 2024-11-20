107f29e31324c6af99d84d91d21b001ee883fb91
soren <ghorbanpoopooria@gmail.com>
2024-11-20 17:14:00 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <p class="tittle"> ASEMAN </p>
+        <nav class="main-menu">
+            <ul>
+                <li><a>foroshgah</a></li>
+                <li><a>ertebat</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <img src="Untitled4-1.png" alt="Untitled4-1.png">
+
+    </main>
+    <footer>
+        
+
+    </footer>
+    
+</body>
+</html>
